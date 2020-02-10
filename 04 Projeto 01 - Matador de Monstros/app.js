40f79ef2a40a62d0b80e9a8c812fb9f1410b7bfc
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-02-10 11:53:29 -0500

    Implementando Atacar

--- 04 Projeto 01 - Matador de Monstros/app.js
+++ 04 Projeto 01 - Matador de Monstros/app.js
@@ -15,9 +15,25 @@ new Vue({
             this.running = true;
             this.playerLife = 100;
             this.monsterLife = 100;
+        },
+        hurt(prop, min, max, special) {
+            const plus = special ? 5 : 0;
+            const hurt = this.getRandom(min+plus, max+plus);
+            this[prop] = Math.max(this[prop] - hurt, 0); //Evita que a vida seja negativa
+        },
+        attack(special) {
+            this.hurt('monsterLife', 5, 10, special);
+            this.hurt('playerLife', 7, 12, false);
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min;
+            return Math.round(value);
         }
     },
     watch: {
-
+        hasResult(value) {
+            if(value)
+                this.running = false;
+        }
     }
 });

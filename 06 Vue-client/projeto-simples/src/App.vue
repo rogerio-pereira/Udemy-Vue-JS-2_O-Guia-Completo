8c8b8ba40ef89ed95383ba0dca1fda83260b6673
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-02-11 10:41:51 -0500

    Entendendo os arquivos Vue

--- 06 Vue-client/projeto-simples/src/App.vue
+++ 06 Vue-client/projeto-simples/src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+    {{titulo}}
+
+    <button @click='titulo += "#"'>Alterar Titulo</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data: function() {
+      return {
+        titulo: 'Teste Data usando VueJS'
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    background-color: chocolate;
+    color: white;
+  }
 </style>

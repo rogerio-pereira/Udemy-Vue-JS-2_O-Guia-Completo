f76c3be111cf8e8ca620426c0fa08c4f9c8fef18
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-02-12 16:25:08 -0500

    Componente Lista de Tarefas pt2

--- 10 Projeto 2 - Lista de Tarefas (ToDo)/tarefas/src/components/TaskGrid.vue
+++ 10 Projeto 2 - Lista de Tarefas (ToDo)/tarefas/src/components/TaskGrid.vue
@@ -1,8 +1,11 @@
 <template>
     <div class='task-grid'>
-        <template>
+        <template v-if='tasks.length'>
             <task v-for='task in tasks' :key='task.name' :task='task' />
         </template>
+        <p class="no-task" v-else>
+            Sua vida está em dia ;)
+        </p>
     </div>
 </template>
 
@@ -24,5 +27,18 @@
 </script>
 
 <style scoped>
+    .task-grid {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
 
+    .task-grid .task {
+        margin: 10px;
+    }
+
+    .no-task {
+        color: #aaa;
+        font-size: 1.7rem;
+    }
 </style>

39e6890b7088b96fee3cb6a7a893bceb3cf780d2
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-02-12 16:19:35 -0500

    Componente Tarefa

--- 10 Projeto 2 - Lista de Tarefas (ToDo)/tarefas/src/components/TaskGrid.vue
+++ 10 Projeto 2 - Lista de Tarefas (ToDo)/tarefas/src/components/TaskGrid.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class='tasks'>
+    <div class='task-grid'>
         <template>
-            <div v-for='task in tasks' :key='task.name'>
-                {{task.name}}
-            </div>
+            <task v-for='task in tasks' :key='task.name' :task='task' />
         </template>
     </div>
 </template>
 
 <script>
+    import Task from '@/components/Task'
     export default {
+        components: {
+            Task,
+        },
         props: {
             tasks: {type: Array, required: true}
         },

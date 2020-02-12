55f836a24698f0fff258bc3787fc0552a833d458
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-02-12 10:05:39 -0500

    Validando Props parte 2

--- 08 Comunicação entre componentes/comunicacao/src/components/UsuarioInfo.vue
+++ 08 Comunicação entre componentes/comunicacao/src/components/UsuarioInfo.vue
@@ -13,7 +13,11 @@ export default {
         nome: {
             type: String,
             //required: true,
-            default: 'Anônimo'
+            default: 'Anônimo',
+            /*default: function() {
+                return Array(10).fill(0).join(',');
+            }*/
+        }
     },
     methods: {
         inverterNome() {

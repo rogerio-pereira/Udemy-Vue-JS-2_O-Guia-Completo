4abb1d03fd7eb538c00835e6b54ab94b883f0e8a
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-02-17 09:43:45 -0500

    Componente nova tarefa pt-2

--- 10 Projeto 2 - Lista de Tarefas (ToDo)/tarefas/src/components/NewTask.vue
+++ 10 Projeto 2 - Lista de Tarefas (ToDo)/tarefas/src/components/NewTask.vue
@@ -1,7 +1,8 @@
 <template>
     <div class='new-task'>
-        <input type='text' v-model='name' @keydown.enter='add'>
-        <button @click='add'>+</button>
+        <input type='text' v-model='name' @keydown.enter='add' class='form-element' placeholder='Nova Tarefa'>
+        
+        <button @click='add' class='form-element'>+</button>
     </div>
 </template>
 
@@ -22,5 +23,29 @@
 </script>
 
 <style scoped>
+    .new-task {
+        margin: 35px;
+    }
+    
+    .form-element {
+        outline: none;
+        font-size: 2rem;
+        border: 1px solid white;
+        padding: 5px 10px 8px;
+        color: white;
+    }
 
+    input.form-element {
+        width: 500px;
+        background-color: #fff2;
+        border-top-left-radius: 8px;
+        border-bottom-left-radius: 8px;
+    }
+
+    button.form-element {
+        border-left: none;
+        background-color: #2196f3;
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+    }
 </style>

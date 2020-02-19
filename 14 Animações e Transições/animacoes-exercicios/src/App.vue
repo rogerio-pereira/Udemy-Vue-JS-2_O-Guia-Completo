3a1d7ea7f6035ea845c682d1c978e1b01fce7163
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-02-19 13:13:54 -0500

    Usando v-show

--- 14 Animações e Transições/animacoes-exercicios/src/App.vue
+++ 14 Animações e Transições/animacoes-exercicios/src/App.vue
@@ -4,12 +4,12 @@
 		<hr>
 		
 		<b-button variant='primary' class='mb-4' @click='exibir = !exibir'>Mostrar Mensagem</b-button>
-		<!--<transition name='fade'>
+		<transition name='fade'>
 			<b-alert variant='info' show v-if='exibir'>{{msg}}</b-alert>
-		</transition>-->
+		</transition>
 
 		<transition name='slide' type='animation'>
-			<b-alert variant='info' show v-if='exibir'>{{msg}}</b-alert>
+			<b-alert variant='info' show v-show='exibir'>{{msg}}</b-alert>
 		</transition>
 	</div>
 </template>
@@ -62,7 +62,7 @@ export default {
 }
 .slide-leave-active {
 	animation: slide-out 2s ease;
-	transition: opacity 6s;
+	transition: opacity 2s;
 }
 
 .slide-enter,

2e397656051c2664ab5bcfe63769465e07a1526c
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-02-19 16:03:29 -0500

    Usando <transition-move> para animar Listas

--- 14 Animações e Transições/animacoes-exercicios/src/App.vue
+++ 14 Animações e Transições/animacoes-exercicios/src/App.vue
@@ -60,9 +60,11 @@
 
 		<hr>
 		<b-button @click='adicionarAluno' class='mb-4'>Adicionar Aluno</b-button>
-		<b-list-group v-for="(aluno, i) in alunos" :key="aluno">
-			<b-list-group-item @click='removerAluno(i)'>{{aluno}}</b-list-group-item>
-		</b-list-group>
+		<transition-group name='slide' tag='div'>
+			<b-list-group v-for="(aluno, i) in alunos" :key="aluno">
+				<b-list-group-item @click='removerAluno(i)'>{{aluno}}</b-list-group-item>
+			</b-list-group>
+		</transition-group>
 	</div>
 </template>
 
@@ -181,6 +183,8 @@ export default {
 	transition: opacity 2s;
 }
 .slide-leave-active {
+	position: absolute;
+	width: 100%;
 	animation: slide-out 2s ease;
 	transition: opacity 2s;
 }
@@ -188,5 +192,9 @@ export default {
 .slide-enter,
 .slide-leave-to {
 	opacity: 0;
+}
+
+.slide-move {
+	transition: transform 1s;
 }
 </style>

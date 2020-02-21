0c71c4a868b69e0d104cda2c15b69b5d30b84245
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-02-21 11:18:00 -0500

    Reagindo a mudanças de parametros de rotas

--- 16 Vue-Router/rotas-exercicios/src/components/template/Menu.vue
+++ 16 Vue-Router/rotas-exercicios/src/components/template/Menu.vue
@@ -6,8 +6,11 @@
                 <a>Inicio</a>
             </router-link>
             <!-- Não usamos o exact aqui pq uma rota /usuario/1/editar ficará ativo do mesmo modo -->
-            <router-link to='/usuario/10' tag='li' active-class='active'>
-                <a>Usuario</a>
+            <router-link to='/usuario/1' tag='li' active-class='active'>
+                <a>Usuario1</a>
+            </router-link>
+            <router-link to='/usuario/2' tag='li' active-class='active'>
+                <a>Usuario2</a>
             </router-link>
         </ul>
     </nav>

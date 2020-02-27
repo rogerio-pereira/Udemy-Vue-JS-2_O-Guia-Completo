3226a0c0dc818372362d9b8f5d22f3778f3485e9
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-02-27 11:54:26 -0500

    Criando primeiros componentes

---
+++ 18 Projeto Stock Trader/stock-trader/src/components/portfolio/Stock.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Ação (Portfolio)</h1>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

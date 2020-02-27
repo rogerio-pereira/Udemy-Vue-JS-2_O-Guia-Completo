8130598279c8a119b2c694b3a322af68c80c8f6e
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-02-27 14:31:15 -0500

    Trabalhando com ações do Portfolio

--- 18 Projeto Stock Trader/stock-trader/src/components/stocks/Stock.vue
+++ 18 Projeto Stock Trader/stock-trader/src/components/stocks/Stock.vue
@@ -42,7 +42,7 @@
                     quantity: this.quantity
                 }
 
-                console.log(order)
+                this.$store.dispatch('buyStock', order)
                 this.quantity = 0
             }
         }

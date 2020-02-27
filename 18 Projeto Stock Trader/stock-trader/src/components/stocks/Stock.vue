b2b7723ff645d0bf601739b2f0e27642374238bc
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-02-27 13:30:51 -0500

    Criando o componente Ações (Stocks)

--- 18 Projeto Stock Trader/stock-trader/src/components/stocks/Stock.vue
+++ 18 Projeto Stock Trader/stock-trader/src/components/stocks/Stock.vue
@@ -1,5 +1,21 @@
 <template>
-    <h1>Ação</h1>
+    <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+        <v-card class="green darken-3 white--text">
+            <v-card-title class="headline">
+                <strong>
+                    Nome
+                    <small>Preço: Preço</small>
+                </strong>
+            </v-card-title>
+        </v-card>
+
+        <v-card>
+            <v-container fill-height>
+                <v-text-field label='Quantidade' type='number' />
+                <v-button class="green darken-3 white--text">Comprar</v-button>
+            </v-container>
+        </v-card>
+    </v-flex>
 </template>
 
 <script>
--- 18 Projeto Stock Trader/stock-trader/src/components/stocks/Stocks.vue
+++ 18 Projeto Stock Trader/stock-trader/src/components/stocks/Stocks.vue
@@ -1,12 +1,23 @@
 <template>
-    <h1>Ações</h1>
+    <v-layout row wrap>
+        <stock v-for='stock in stocks' :key='stock.id' />
+    </v-layout>
 </template>
 
 <script>
+    import Stock from './Stock'
     export default {
+        components: {
+            Stock
+        },
         data() {
             return {
-
+                stocks: [
+                    {id: 1, name: 'BMW', preco: 110},
+                    {id: 2, name: 'Google', preco: 200},
+                    {id: 3, name: 'Apple', preco: 280},
+                    {id: 4, name: 'Twitter', preco: 83},
+                ]
             }
         }
     }

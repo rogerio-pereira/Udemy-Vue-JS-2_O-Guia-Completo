3c8912625febb12a7efd225080628c39da7f0e3c
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-03-02 14:47:52 -0500

    Monty Hall 02

--- 20 Monty Hall/monty-hall/src/App.vue
+++ 20 Monty Hall/monty-hall/src/App.vue
@@ -1,11 +1,19 @@
 <template>
     <div id="app">
         <h1>Problema de Monty Hall</h1>
+
+        <gift />
     </div>
 </template>
 
 <script>
+    import Gift from './components/Gift'
+
     export default {
+        name: 'App',
+        components: {
+            Gift
+        },
         data() {
             return {
 
